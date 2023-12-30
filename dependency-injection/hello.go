b9d099c76bee2c296457e98d9375e61ac7057d55
Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>
2023-12-30 13:26:33 +0530

    add dependency injection

---
+++ dependency-injection/hello.go
@@ -0,0 +1,15 @@
+package dependencyinjection
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func Greet(w io.Writer, name string) {
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Chris")
+}
